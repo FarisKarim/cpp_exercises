5825a855e09be6489b326baee73a8c64b2cd683a
Faris <fariskarim2000@gmail.com>
2025-05-12 17:19:45 -0400

    end 8.8

---
+++ 8/main.cpp
@@ -0,0 +1,93 @@
+#include <iostream>
+
+// 8.6 QUIZ - SWITCH STATEMENTS
+// int calculate(int x, int y, char op)
+// {
+//     switch (op) {
+//         case '+':
+//             return x + y;
+//         case '-':
+//             return x - y;
+//         case '*':
+//             return x * y;
+//         case '/':
+//             return x / y;
+//         case '%':
+//             return x % y;
+//         default:
+//             std::cout << "Invalid operator. Returning ";
+//             return 0;
+//     }
+// }
+
+// int main() {
+//     std::cout << "Enter an integer: ";
+//     int num1 {};
+//     std::cin >> num1;
+//     std::cout << "Enter another integer: ";
+//     int num2 {};
+//     std::cin >> num2;
+//     char op {};
+//     std::cout << "Enter an operator: ";
+//     std::cin >> op;
+//     std::cout << num1 << " " << op << " " << num2 << " is " << calculate(num1, num2, op) << '\n';
+
+// }
+// #include <cmath> // for sqrt() function
+
+// int main()
+// {
+//     double x{};
+// tryAgain: // this is a statement label
+//     std::cout << "Enter a non-negative number: ";
+//     std::cin >> x;
+//     if (x < 0.0)
+//         goto tryAgain; // this is the goto statement
+//     std::cout << "The square root of " << x << " is " << std::sqrt(x) << '\n';
+//     return 0;
+// }
+
+// int main()
+// {
+//     char az_char{'a'};
+//     while (az_char <= 'z')
+//     {
+//         std::cout << az_char << " " << static_cast<int>(az_char) << "\n";
+//         ++az_char;
+//     }
+// }
+
+// 8.8 QUIZ
+// Loop between 5 and 1
+// int main()
+// {
+//     int outer{5};
+//     while (outer >= 1)
+//     {
+//         int inner = {outer};
+//         while (inner >= 1)
+//         {
+//             std::cout << inner << ' ';
+//             --inner;
+//         }
+//         std::cout << '\n';
+//         --outer;
+//     }
+//     return 0;
+// }
+
+// int main() {
+//     int outer {1};
+//     while (outer <= 5) {
+//         int inner {5};
+//         while (inner >= 1) {
+//             if (inner <= outer)
+//                 std::cout << inner << ' ';
+//             else
+//                 std::cout << "  ";
+//             --inner;
+//         }
+//         std::cout << '\n';
+//         ++outer;
+//     }
+// }
