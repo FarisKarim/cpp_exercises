98a2f73a9a5f07cc78b89574dfd8037410fd9f69
Faris <fariskarim2000@gmail.com>
2025-05-13 19:45:30 -0400

    chapter 10 finished

---
+++ 10/main.cpp
@@ -0,0 +1,30 @@
+// type alias 10.7
+// using PrintError = int;
+
+// PrintError printData();
+
+#include <iostream>
+
+namespace constants
+{
+    constexpr double pi{3.14159};
+}
+
+using Degrees = double;
+using Radians = double;
+Radians convertToRadians(Degrees degrees)
+{
+    return degrees * constants::pi / 180;
+}
+
+int main()
+{
+    std::cout << "Enter a number of degrees: ";
+    Degrees degrees{};
+    std::cin >> degrees;
+
+    Radians radians{convertToRadians(degrees)};
+    std::cout << degrees << " degrees is " << radians << " radians.\n";
+
+    return 0;
+}
