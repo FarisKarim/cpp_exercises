624a418ab57a0a6184d531f22d2f58d2ca6bd7ff
Faris <fariskarim2000@gmail.com>
2025-05-18 21:19:33 -0400

    end 12.9

--- 12/main.cpp
+++ 12/main.cpp
@@ -89,3 +89,63 @@ void printValue(int& y) // y only accepts modifiable lvalues
 
 //     return 0;
 // }
+
+// #include <iostream>
+
+// int main()
+// {
+//     int x{ 5 };
+//     std::cout << x << '\n';  // print the value of variable x
+//     std::cout << &x << '\n'; // print the memory address of variable x
+
+//     std::cout << *(&x) << '\n'; // print the value at the memory address of variable x (parentheses not required, but make it easier to read)
+
+//     return 0;
+// }
+
+// int main()
+// {
+//     int x { 5 };    // normal variable
+//     int& ref { x }; // a reference to an integer (bound to x)
+
+//     int* ptr;       // a pointer to an integer
+
+//     return 0;
+// }
+// int main()
+// {
+//     int v{ 5 };
+
+//     int* ptr0 { &v };             // points to an "int" but is not const itself.  We can modify the value or the address.
+//     const int* ptr1 { &v };       // points to a "const int" but is not const itself.  We can only modify the address.
+//     int* const ptr2 { &v };       // points to an "int" and is const itself.   We can only modify the value.
+//     const int* const ptr3 { &v }; // points to a "const int" and is const itself.  We can't modify the value nor the address.
+
+//     // if the const is on the left side of the *, the const belongs to the value
+//     // if the const is on the right side of the *, the const belongs to the pointer
+
+//     return 0;
+// }
+
+#include <iostream>
+#include <string>
+
+void printByValue(std::string val) // The function parameter is a copy of str
+{
+    std::cout << val << '\n'; // print the value via the copy
+}
+
+void printByReference(const std::string& ref) // The function parameter is a reference that binds to str
+{
+    std::cout << ref << '\n'; // print the value via the reference
+}
+
+int main()
+{
+    std::string str{ "Hello, world!" };
+
+    printByValue(str); // pass str by value, makes a copy of str
+    printByReference(str); // pass str by reference, does not make a copy of str
+
+    return 0;
+}
