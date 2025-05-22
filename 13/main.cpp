8e718970c12611eeca4622e0bdd06d2f97d4728e
Faris <fariskarim2000@gmail.com>
2025-05-22 11:47:23 -0400

    end 14.5

--- 13/main.cpp
+++ 13/main.cpp
@@ -220,31 +220,32 @@
 //     Pair<double> p2{ 1.2, 3.4 };
 //     std::cout << max(p2) << " is larger\n"; // call to max<double> using template argument deduction (prefer this)
 
-//     return 0;
+// //     return 0;
+// // }
+
+// template <typename T>
+// struct Triad {
+//     T first {};
+//     T second {};
+//     T third {};
+// };
+
+// template <typename T>
+// Triad(T,T,T) -> Triad<T>;
+
+// template <typename T>
+// void print(Triad<T>& t ) {
+//     std::cout << "[" << t.first << ", " << t.second << ", " << t.third << "]";
 // }
 
-template <typename T>
-struct Triad {
-    T first {};
-    T second {};
-    T third {};
-};
+// int main()
+// {
+// 	Triad t1{ 1, 2, 3 }; // note: uses CTAD to deduce template arguments
+// 	print(t1);
 
-template <typename T>
-Triad(T,T,T) -> Triad<T>;
+// 	Triad t2{ 1.2, 3.4, 5.6 }; // note: uses CTAD to deduce template arguments
+// 	print(t2);
+    
 
-template <typename T>
-void print(Triad<T>& t ) {
-    std::cout << "[" << t.first << ", " << t.second << ", " << t.third << "]";
-}
-
-int main()
-{
-	Triad t1{ 1, 2, 3 }; // note: uses CTAD to deduce template arguments
-	print(t1);
-
-	Triad t2{ 1.2, 3.4, 5.6 }; // note: uses CTAD to deduce template arguments
-	print(t2);
-
-	return 0;
-}
+// 	return 0;
+// }
